bc1e923ebf0af5e2428b69fc644c9f5fc9041ce1
DiogoFabricioAG <diogo.abregu.g@uni.pe>
2025-01-03 12:18:12 -0500

    made with love

---
+++ components/AppAlert.vue
@@ -0,0 +1,5 @@
+<template>
+    <span class="text-red-500 bg-red-100 border border-red-400 px-4 py-2 rounded">
+        <slot />
+    </span>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+    <section>
+      <p>This page will be displayed at the /about route.</p>
+    </section>
+    <AppAlert>
+        Posiblemente
+    </AppAlert>
+  </template>
